3fb36b9599bdd3b8d3d89a1bca56f38ad47118bb
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2020-11-15 15:54:26 +0800

    UpdateCode And AddNotes

--- src/com/github/thecinfiniteteam/infinite_builder/main.py
+++ src/com/github/thecinfiniteteam/infinite_builder/main.py
@@ -1,29 +1,30 @@
 #encoding: utf-8
+#导入库
 from sys import builtin_module_names
 #from com.github.thecinfiniteteam.infinite_builder import *
 import os, zipfile, sys, json, time, socket, socketserver
 
-class Build(object):
-    def __init__(self):
-        self.name = 'Infinite Builder Tool'
-        self.id = 'infinite_builder_tool'
-        self.version = 'b1'
-        self.runDir = os.getcwd()
+class Build(object):#创建类
+    def __init__(self):#创建初始化方法
+        self.name = 'Infinite Builder Tool'#定义name
+        self.id = 'infinite_builder_tool'#定义id
+        self.version = 'b1'#定义版本
+        self.runDir = os.getcwd()#定义运行路径
 
-    def __str__(self):
-        return 'Infinite Builder Tool'
+    def __str__(self):#如果以str输出会调用这个方法
+        return self.name#返回一下软件名字
 
-    def getInformation(self):
-        returnJson = {
+    def getInformation(self):#创建获取信息方法
+        returnJson = { #创建信息字典
             'NAME': self.name,
             'ID': self.id,
             'VERSION': self.version,
             'RUN_DIR': self.runDir,
             'NOW_TIME': time.time()
         }
-        return returnJson
+        return returnJson#返回信息字典
 
-    def getHelpInformation(self):
+    def getHelpInformation(self):#创建获取帮助信息方法
         self.help = """
         -|*Infinite Builder Tool Help Information*|-
         Note: the use of this software caused by the user to bear, the author's team has no responsibility
@@ -33,18 +34,18 @@ class Build(object):
         注意:使用此软件造成的事物由使用者承担 作者团队无任何责任
         -------------------------
         """
-        return self.help
+        return self.help#返回帮助消息
 
-    def comInitial(self):
-        pass
+    def comInitial(self):#创建初始化组件
+        pass#TODO
 
-    def comRunning(self):
-        pass
+    def comRunning(self):#创建运行组件
+        pass#TODO
 
-    def runningPack(self):
-        print(self.getHelpInformation())
+    def runningPack(self):#创建运行'整合包'
+        print(self.getHelpInformation())#打印帮助信息
 
 if __name__ == "__main__":
-    buildToolClass = Build()
+    buildToolClass = Build()#实例化Build类
 
-    buildToolClass.runningPack()
+    buildToolClass.runningPack()#运行'整合包'
