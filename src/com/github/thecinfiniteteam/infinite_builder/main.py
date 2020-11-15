d60463999e861dce499c25c7bd2bd71ee04b13d2
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2020-11-15 15:40:51 +0800

    UpdateCode

--- src/com/github/thecinfiniteteam/infinite_builder/main.py
+++ src/com/github/thecinfiniteteam/infinite_builder/main.py
@@ -1,14 +1,14 @@
 #encoding: utf-8
-from com.github.thecinfiniteteam.infinite_builder import *
+from sys import builtin_module_names
+#from com.github.thecinfiniteteam.infinite_builder import *
 import os, zipfile, sys, json, time, socket, socketserver
 
-class Build():
+class Build(object):
     def __init__(self):
         self.name = 'Infinite Builder Tool'
         self.id = 'infinite_builder_tool'
         self.version = 'b1'
         self.runDir = os.getcwd()
-        self.pythonVersion = sys.version()
 
     def __str__(self):
         return 'Infinite Builder Tool'
@@ -19,7 +19,32 @@ class Build():
             'ID': self.id,
             'VERSION': self.version,
             'RUN_DIR': self.runDir,
-            'PYTHON_VERSION': self.pythonVersion,
             'NOW_TIME': time.time()
         }
         return returnJson
+
+    def getHelpInformation(self):
+        self.help = """
+        -|*Infinite Builder Tool Help Information*|-
+        Note: the use of this software caused by the user to bear, the author's team has no responsibility
+        --------------------------------------------
+
+        -|*无限构建工具帮助信息*|-
+        注意:使用此软件造成的事物由使用者承担 作者团队无任何责任
+        -------------------------
+        """
+        return self.help
+
+    def comInitial(self):
+        pass
+
+    def comRunning(self):
+        pass
+
+    def runningPack(self):
+        print(self.getHelpInformation())
+
+if __name__ == "__main__":
+    buildToolClass = Build()
+
+    buildToolClass.runningPack()
